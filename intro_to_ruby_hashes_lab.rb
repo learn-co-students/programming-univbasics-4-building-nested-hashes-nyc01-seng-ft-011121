7936d1d2e3a63c9f38c4529ccddfd44b871a832b
Karris  Black <karris.b@gmail.com>
2021-01-02 18:23:31 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,4 +1,7 @@
 def base_hash
+  {
+    :railroads => {}
+  }
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 
@@ -9,7 +12,11 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-
+  hash = base_hash 
+  hash[:railroads][:pieces] = 4
+  hash[:railroads][:rent_in_dollars] = {}
+  hash[:railroads][:names] = {}
+  hash
 end
 
 def monopoly_with_third_tier
@@ -17,7 +24,17 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-
+  hash = monopoly_with_second_tier
+  hash[:railroads][:rent_in_dollars][:one_piece_owned] = 25
+  hash[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
+  hash[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
+  hash[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
+  hash[:railroads][:names][:reading_railroad] = {}
+  hash[:railroads][:names][:pennsylvania_railroad] = {}
+  hash[:railroads][:names][:b_and_o_railroad] = {}
+  hash[:railroads][:names][:shortline_railroad] = {}
+  hash
+        
 end
 
 def monopoly_with_fourth_tier
@@ -25,5 +42,10 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-
+  hash = monopoly_with_third_tier
+  hash[:railroads][:names][:reading_railroad][:mortgage_value] = 100
+  hash[:railroads][:names][:pennsylvania_railroad][:mortgage_value] = 200
+  hash[:railroads][:names][:b_and_o_railroad][:mortgage_value] = 400
+  hash[:railroads][:names][:shortline_railroad][:mortgage_value] = 800
+  hash
 end
